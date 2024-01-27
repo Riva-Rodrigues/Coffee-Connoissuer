8172d78e98eb25b1ffb4b47b7018b26ccd5ecebc
Riva <rodriguesriva1130@gmail.com>
2024-01-27 12:27:31 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+
+
